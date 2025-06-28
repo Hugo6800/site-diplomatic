cf993c4d4cc712d45f391a685308809d80f2fa79
Angelique <angelique.copere@gmail.com>
2025-06-28 15:16:47 +0200

    update design footer

--- app/components/Footer/Infos.tsx
+++ app/components/Footer/Infos.tsx
@@ -1,6 +1,6 @@
 export default function Infos() {
     return (
-        <div className="flex justify-between items-center gap-4 w-full">
+        <div className="flex justify-between font-neulisalt items-center gap-4 w-full">
             <p>{`Média indépendant dédié à l'analyse géopolitique mondiale.`}</p>   
             <p>© The Diplomatic Post 2025 - Tous droits réservés</p>
         </div>
--- app/components/Footer/Links.tsx
+++ app/components/Footer/Links.tsx
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export default function Links() {
     return (
-        <ul className="grid grid-cols-2 md:w-full md:flex md:justify-around items-center">
-            <li className="text-accent hover:font-bold transition-colors">
+        <ul className="grid grid-cols-2 md:w-full md:flex md:justify-around items-center font-neulisalt">
+            <li className="text-[#DE595C] hover:font-bold transition-colors">
                 <Link href="/about">À propos</Link>
             </li>
-            <li className="text-accent hover:font-bold transition-colors">
+            <li className="text-[#DE595C] hover:font-bold transition-colors">
                 <Link href="/articles">Articles</Link>
             </li>
-            <li className="text-accent hover:font-bold transition-colors">
+            <li className="text-[#DE595C] hover:font-bold transition-colors">
                 <Link href="/contact">Contact</Link>
             </li>
-            <li className="text-accent hover:font-bold transition-colors">
+            <li className="text-[#DE595C] hover:font-bold transition-colors">
                 <Link href="/mentions-legales">Mentions légales</Link>
             </li>
         </ul>
