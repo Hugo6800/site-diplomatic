1cc9ddbd8b000bd7017fab040c2a7c3baecc726a
Angelique <angelique.copere@gmail.com>
2025-07-13 14:18:59 +0200

    add page ateliers

---
+++ app/ateliers/page.tsx
@@ -0,0 +1,7 @@
+export default function Ateliers() {
+    return (
+        <main className="px-6 md:px-24 xl:px-64 mt-28 lg:mt-48 mb-20">
+            <h2 className="font-bold font-fractul text-4xl lg:text-5xl mb-5">Ateliers</h2>
+        </main>
+    )
+}
