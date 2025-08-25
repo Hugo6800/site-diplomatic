4c8b31d9f4f52a76bad57b329a74d78bad609e39
Angelique <angelique.copere@gmail.com>
2025-08-25 16:13:28 +0200

    delete unused categories

--- app/utils/categories.ts
+++ app/utils/categories.ts
@@ -1,14 +1,8 @@
 export function getColorCircle(category: string): string {
     const colors: { [key: string]: string } = {
         'Politique': '#FF8989',
-        'Économie': '#89FFA2',
         'Société': '#89D5FF',
         'Culture': '#FFE289',
-        'Sport': '#FF89E4',
-        'Science': '#D489FF',
-        'Technologie': '#89FFE8',
-        'Santé': '#FF89B0',
-        'Environnement': '#B0FF89',
         'International': '#FFC289'
     };
 
