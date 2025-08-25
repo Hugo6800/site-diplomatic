6b6cce74b291fb11ba46bee123fe0a976bf14c16
Angelique <angelique.copere@gmail.com>
2025-08-25 14:47:28 +0200

    add crusor

--- app/components/JobFull.tsx
+++ app/components/JobFull.tsx
@@ -5,7 +5,7 @@ export default function JobFull({ title, shortDescription, postDescription, sear
                 <h2 className="font-bold font-fractul text-[34px] mb-4 dark:text-white w-fit">{title}</h2>
                 <button
                     onClick={() => window.location.href = `/applyJob?jobId=${id}`}
-                    className="ml-auto font-bold font-neulisalt bg-[#F3DEDE] dark:bg-[#1E1E1E] flex justify-center items-center rounded-2xl px-4 py-2 italic text-[1rem] mb-4 dark:text-white w-fit">
+                    className="ml-auto cursor-pointer font-bold font-neulisalt bg-[#F3DEDE] dark:bg-[#1E1E1E] flex justify-center items-center rounded-2xl px-4 py-2 italic text-[1rem] mb-4 dark:text-white w-fit">
                     Postuler
                 </button>
             </div>
