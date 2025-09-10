eaaa6a629a0d177aea45a1cb3cad1510d04bd7b1
Angelique <angelique.copere@gmail.com>
2025-09-10 14:58:15 +0200

    update page mentions

--- app/mentions/page.tsx
+++ app/mentions/page.tsx
@@ -5,14 +5,19 @@ export default function Mentions() {
         <main className="flex flex-col gap-4 px-6 md:px-24 xl:px-64 mt-28 lg:mt-48 mb-20">
             <h2 className="font-bold font-fractul text-5xl dark:text-[#C5B0B0]">Mentions légales</h2>
             <p className="font-neulisalt dark:text-[#F4DFDF]">
-                {`Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance
-                en l'économie numérique, il est précisé aux utilisateurs du site The Diplomatic Post
-                l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi :`}
+                {`Conformément aux dispositions de la loi n°2004-575 du 21 juin 2004 pour la Confiance en l’économie numérique,
+                il est porté à la connaissance des utilisateurs et visiteurs, ci-après l' "Utilisateur", du site
+                diplomaticpost.fr , ci-après le "Site", les présentes mentions légales.
+                La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans
+                réserve des présentes mentions légales.`}
             </p>
             <div className="flex flex-col">
-                <h3 className="font-bold font-fractul text-lg mb-2 dark:text-[#F4DFDF]">Éditeur du site</h3>
+                <h3 className="font-bold font-fractul text-lg mb-2 dark:text-[#F4DFDF]">Éditeurs du site</h3>
                 <p className="font-neulisalt dark:text-[#F4DFDF]">
-                    {`Angélique Copéré`}
+                    {`Propriétaire du site : Hugo Lambert`}
+                </p>
+                <p className="font-neulisalt dark:text-[#F4DFDF]">
+                    {`Développement du site : Angélique Copéré`}
                 </p>
                 <p className="font-neulisalt dark:text-[#F4DFDF]">
                     {`Email : angelique.copere@free-dev.fr`}
@@ -33,6 +38,15 @@ export default function Mentions() {
                 <Link href="https://www.netlify.com" target="_blank" className="font-neulisalt dark:text-[#F4DFDF]">
                     {`netlify.com`}
                 </Link>
+            </div>
+            <div className="flex flex-col">
+                <h3 className="font-bold font-fractul text-lg mb-2 dark:text-[#F4DFDF]">Accès au site</h3>
+                <p className="font-neulisalt dark:text-[#F4DFDF]">
+                    {`Le Site est normalement accessible, à tout moment, à l'Utilisateur. Toutefois, l'Editeur pourra, à tout
+                    moment, suspendre, limiter ou interrompre le Site afin de procéder, notamment, à des mises à jour ou
+                    des modifications de son contenu. L'Editeur ne pourra en aucun cas être tenu responsable des
+                    conséquences éventuelles de cette indisponibilité sur les activités de l'Utilisateur.`}
+                </p>
             </div>
             <div className="flex flex-col">
                 <h3 className="font-bold font-fractul text-lg mb-2 dark:text-[#F4DFDF]">Stockages des données</h3>
@@ -57,12 +71,31 @@ export default function Mentions() {
                 </p>
             </div>
             <div className="flex flex-col">
+                <h3 className="font-bold font-fractul text-lg mb-2 dark:text-[#F4DFDF]">Collecte des données</h3>
+                <p className="font-neulisalt dark:text-[#F4DFDF]">
+                    {`Le Site assure à l'Utilisateur une collecte et un traitement des données personnelles dans le respect
+                    de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers
+                    aux libertés et dans le respect de la règlementation applicable en matière de traitement des données
+                    à caractère personnel conformément au règlement (UE) 2016/679 du Parlement européen et du
+                    Conseil du 27 avril 2016 (ci-après, ensemble, la "Règlementation applicable en matière de
+                    protection des Données à caractère personnel").
+                    En vertu de la Règlementation applicable en matière de protection des Données à caractère
+                    personnel, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de
+                    ses données personnelles. L'Utilisateur peut exercer ce droit :`}
+                </p>
+                <ul className="list-disc pl-6 font-neulisalt dark:text-[#F4DFDF]">
+                    <li>{`par mail à l'adresse email diplomaticpost@outlook.com`}</li>
+                    <li>{`depuis le formulaire de contact ;`}</li>
+                    <li>{`depuis son espace personnel ;`}</li>
+                </ul>
+            </div>
+            <div className="flex flex-col">
                 <h3 className="font-bold font-fractul text-lg mb-2 dark:text-[#F4DFDF]">Données personnelles</h3>
                 <p className="font-neulisalt dark:text-[#F4DFDF]">
                     {`The Diplomatic Post s'engage à ce que la collecte et le traitement de vos données,
                     effectués à partir du site, soient conformes au règlement général sur la protection des données (RGPD) et à la loi Informatique et Libertés.
                     Pour toute information ou exercice de vos droits Informatique et Libertés sur les traitements de données personnelles gérés par The Diplomatic Post,
-                    vous pouvez nous contacter par email à l'adresse : privacy@diplomaticpost.com.`}
+                    vous pouvez nous contacter par email à l'adresse : diplomaticpost@outlook.fr.`}
                 </p>
             </div>
             <div className="flex flex-col">
@@ -84,7 +117,7 @@ export default function Mentions() {
             <div className="flex flex-col">
                 <h3 className="font-bold font-fractul text-lg mb-2 dark:text-[#F4DFDF]">Contact</h3>
                 <p className="font-neulisalt dark:text-[#F4DFDF]">
-                    {`Pour toute question relative aux présentes mentions légales, vous pouvez nous contacter à l'adresse suivante : contact@diplomaticpost.com.`}
+                    {`Pour toute question relative aux présentes mentions légales, vous pouvez nous contacter à l'adresse suivante : diplomaticpost@outlook.fr.`}
                 </p>
             </div>
         </main>
