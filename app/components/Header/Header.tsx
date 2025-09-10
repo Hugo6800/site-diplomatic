1995659b1dc286f47991a0f5a236e9bf031ca434
Angelique <angelique.copere@gmail.com>
2025-09-10 16:09:38 +0200

    fix bug logo header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -5,22 +5,29 @@ import NavBar from "./NavBar";
 import DarkTheme from "./DarkTheme";
 import Link from "next/link";
 import { User } from "./User";
-import { useTheme } from '@/app/context/ThemeContext';
+// import { useTheme } from '@/app/context/ThemeContext';
 // import PlansButton from "./PlansButton";
 
 export default function Header() {
-    const { isDark } = useTheme();
+    // const { isDark } = useTheme();
 
     return (
         <header className="flex justify-between items-center bg-header dark:bg-background py-5 px-6 md:px-24 xl:px-64 h-24 fixed top-0 left-0 right-0 z-50">
             <NavBar />
             <Link href="/">
                 <Image
-                    src={isDark ? "/Logo_blanc.png" : "/Logo_diplomatic.png"}
+                    src={"/Logo_diplomatic.png"}
                     alt="Logo"
                     width={200}
                     height={100}
-                    className="object-cover"
+                    className="object-cover dark:hidden"
+                />
+                <Image
+                    src={"/Logo_blanc.png"}
+                    alt="Logo"
+                    width={200}
+                    height={100}
+                    className="object-cover hidden dark:block"
                 />
             </Link>
             <div className="hidden lg:flex items-center gap-4">
