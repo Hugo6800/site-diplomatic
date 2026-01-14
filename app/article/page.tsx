cef0176b4ccbd31b47182fe8a86ce7ff27d0897c
Hugo6800 <hugo.lambertfam68@gmail.com>
2026-01-14 15:19:01 +0100

    Update page.tsx

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,5 +1,15 @@
 'use client'
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-48B8PFD2C7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-48B8PFD2C7');
+</script>
+    
 import ArticleContent from '../content/ArticleContent';
 import { Suspense } from 'react';
 
